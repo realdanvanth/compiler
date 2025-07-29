70ecea247a344e70196d8b118c1796db4e7ebdea
realdanvanth <realdanvanth@gmail.com>
2025-07-30 00:01:25 +0530

    Update output.asm

--- src/output.asm
+++ src/output.asm
@@ -1,5 +1,42 @@
 global _start
 _start:
-   mov rax, 60
-   mov rdi, 69
-   syscall
+push rbp
+mov rbp, rsp
+sub rsp,24
+push 50
+push 7
+pop rcx
+pop rax
+xor rdx, rdx
+div rcx
+push rdx
+; expression ends here
+pop rax
+mov [rbp-8], rax
+mov rax, [rbp-8]
+push rax;getting a
+push 20
+pop rax
+pop rbx
+imul rax, rbx
+push rax
+; expression ends here
+pop rax
+mov [rbp-16], rax
+mov rax, [rbp-16]
+push rax;getting b
+push 6
+pop rcx
+pop rax
+xor rdx, rdx
+div rcx
+push rdx
+; expression ends here
+pop rax
+mov [rbp-24], rax
+mov rax, [rbp-24]
+push rax;getting c
+; expression ends here
+pop rdi
+mov rax, 60
+syscall
