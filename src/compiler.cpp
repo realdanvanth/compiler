f7f7d5a7568584a5f17db0135533eccfafe54581
realdanvanth <realdanvanth@gmail.com>
2026-03-16 19:10:34 +0530

    cpp rewrite

---
+++ src/compiler.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+#include <fstream>
+#include <iostream>
+#include <memory>
+using namespace std;
+string readfile(string f) {
+  string out = "";
+  ifstream file(f);
+  string buf;
+  while (getline(file, buf)) {
+    out += buf;
+  }
+  file.close();
+  return out;
+}
+enum tokenType {
+  _type_int,
+  _number,
+  _sysexit,
+  _ident,
+  _semi_colon,
+  _print,
+  _equal,
+  _add,
+  _sub,
+  _mul,
+  _div,
+  _mod,
+  _open_braces,
+  _close_braces,
+  _if,
+  _and,
+  _or,
+  _not,
+  _type_boolean,
+  _boolean,
+  _equal_to,
+  _greater_than,
+  _less_than,
+  _open_curly,
+  _close_curly,
+  _else,
+  _else_if,
+  _while,
+};
+struct Token {
+  tokenType type;
+  string text;
+};
+class Expr {
+public:
+  tokenType op;
+  Expr *left;
+  Expr *right;
+  tokenType val;
+  Expr(tokenType op) { this->op = op; }
+};
+std::vector<Token> tokens;
+void lexer(string code) { // takes string and returns an array of tokens
+  int i = 0;
+  while (i < code.size()) {
+    if (isalpha(code[i])) {
+      string buffer = "";
+      buffer += code[i++];
+      while (i < code.size() &&
+             (isdigit(code[i]) || isalpha(code[i]))) { // var_num --> accepted
+        buffer += code[i++];
+      }
+      if (i < code.size()) { // we read an extra character so headback
+        i--;
+      }
+      if (buffer == "exit") {
+        tokens.push_back({_sysexit, ""});
+        cout << "exit\n";
+      } else {
+        cout << "error\n";
+      }
+    } else if (isdigit(code[i])) { // integers
+      string buffer = "";
+      buffer += code[i++];
+      while (i < code.size() && isdigit(code[i])) {
+        buffer += code[i++];
+      }
+      if (i < code.size()) { // we read an extra character so headback
+        i--;
+      }
+      tokens.push_back({_number, buffer});
+      cout << buffer << "\n";
+    } else if (code[i] == ';') {
+      tokens.push_back({_semi_colon, ""});
+      cout << ";\n";
+    } else if (code[i] == '(') {
+      tokens.push_back({_open_braces, ""});
+      cout << "(\n";
+    } else if (code[i] == ')') {
+      tokens.push_back({_close_braces, ""});
+      cout << ")\n";
+    } else if (code[i] == '+') {
+      tokens.push_back({_add, ""});
+    } else if (code[i] == '-') {
+      tokens.push_back({_sub, ""});
+    } else if (code[i] == '*') {
+      tokens.push_back({_mul, ""});
+    } else if (code[i] == '/') {
+      tokens.push_back({_div, ""});
+    } else if (code[i] == '%') {
+      tokens.push_back({_mod, ""});
+    }
+    i++;
+  }
+}
+
+int main() { lexer(readfile("test.dl")); }
